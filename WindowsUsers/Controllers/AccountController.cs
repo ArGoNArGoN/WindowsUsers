5712a503aa2b731f424456f0a0de4af2a1b963e0
ArGoN <70173258+ArGoNArGoN@users.noreply.github.com>
2022-02-05 12:54:13 +0300

    minor

---
+++ WindowsUsers/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WindowsUsers.Controllers
+{
+    [Route("Users/[controller]")]
+    [ApiController]
+    public class AccountController
+        : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Account()
+        {
+            return base.Content("<div>Hello</div>", "text/html");
+        }
+    }
+}
---
+++ WindowsUsers/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace WindowsUsers.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var fileContents = System.IO.File.ReadAllText("./Views/Index.html");
+            return base.Content(fileContents, "text/html");
+        }
+    }
+}
